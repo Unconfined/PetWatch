b9d850d76840739527df3ff0c0704056ef70af37
Ricky Walker <rickwalk45@gmail.com>
2015-10-08 11:19:32 -0700

    [fix] Made the top margins more consistent across all views

--- client/main.jsx
+++ client/main.jsx
@@ -18,7 +18,8 @@ module.exports = React.createClass({
 		}else{
 			return (
       	<div>
-          <div className="jumbotron container">
+
+          <div className="jumbotron container-fluid">
             <div className="greet">
               <h2>Welcome to PetWatch</h2>
               <p>The most interactive site for your pet sitting needs!</p>
@@ -28,7 +29,9 @@ module.exports = React.createClass({
 							<p>No pet is too big or small!</p>
             </div>
           </div>
-          <div id="carousel-example-generic" className="carousel slide container" data-ride="carousel">
+
+          <div id="carousel-example-generic" className="carousel slide container-fluid" data-ride="carousel">
+
             <div className="carousel-inner" role="listbox">
               <div className="item active">
                 <img src="http://www.extremetech.com/wp-content/uploads/2013/09/4Vln8-640x428.jpg" alt="Slide 1" />
@@ -41,14 +44,11 @@ module.exports = React.createClass({
 							</div>
 							<div className="item">
 								<img src="http://metrouk2.files.wordpress.com/2014/06/ad_136684029.jpg?w=620&h=441&crop=1" alt="Slide 4" />
-
 							</div>
 							<div className="item">
 								<img src="http://metrouk2.files.wordpress.com/2014/06/ad_136683477-e1401803215866.jpg?w=620&h=441&crop=1" alt="Slide 5" />
-
 							</div>
             </div>
-
 
             <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
               <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
@@ -59,6 +59,7 @@ module.exports = React.createClass({
               <span className="sr-only">Next</span>
             </a>
           </div>
+
         </div>
 			)
 		}
