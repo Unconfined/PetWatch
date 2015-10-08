995be25c3cba1e5506485c7e06181fe8084b8d65
Ricky Walker <rickwalk45@gmail.com>
2015-10-08 16:36:37 -0700

    [feat] Change the sitter and owner listing container and image styling

--- client/sitter-profile.jsx
+++ client/sitter-profile.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 module.exports = React.createClass({
 	render: function(){
 		return (
-         <div className='well col-md-6'>
+         <div className='well'>
            {this.renderSitterProfile()}
          </div>
 		)
